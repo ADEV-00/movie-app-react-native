644d8c295353ec0b10dbe0183999c1ae49d4323e
unknown <developer.amel@gmail.com>
2020-12-17 18:27:27 +0100

    Added movieCard component

---
+++ src/components/MovieCard/index.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { View, Text, StyleSheet, ImageBackground } from "react-native";
+
+const MovieCard = ({ title }) => {
+  return (
+    <View style={styles.cardWrapper}>
+      <View style={styles.cardImg}>
+        <ImageBackground
+          style={{ width: "100%", height: "100%" }}
+          source={require("../../assets/img/tenet.jpg")}
+        />
+      </View>
+      <View style={styles.cardDetail}>
+        <Text style={styles.movieTitle}>{title}</Text>
+        <Text style={styles.movieRating}>6.8</Text>
+        <Text style={styles.movieGenre}>Drama, Action</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  cardWrapper: {
+    width: 164,
+    height: 261,
+    backgroundColor: "white",
+    borderRadius: 8,
+    overflow: "hidden",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
+
+    elevation: 12,
+    marginLeft: 16,
+  },
+  cardImg: {
+    width: "100%",
+    height: "60%",
+  },
+  cardDetail: {
+    padding: 16,
+  },
+  movieTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#1A2E42",
+    marginBottom: 4,
+  },
+  movieRating: {
+    fontSize: 12,
+    color: "#333333",
+    marginBottom: 4,
+  },
+  movieGenre: {
+    fontSize: 14,
+    letterSpacing: 2,
+    color: "#333333",
+  },
+});
+
+export default MovieCard;
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,12 +1,61 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  ImageBackground,
+} from "react-native";
+import MovieCard from "../../components/MovieCard";
+
+const { width, height } = Dimensions.get("window");
 
 const HomeScreen = () => {
   return (
-    <View style={{ flex: 1, backgroundColor: "#f5f5f5" }}>
-      <Text>Home Screen</Text>
-    </View>
+    <>
+      <View
+        style={{ backgroundColor: "#f5f5f5", minHeight: height, width: width }}
+      >
+        <ImageBackground
+          source={require("../../assets/img/bg.png")}
+          style={styles.bgImage}
+        />
+        <View style={styles.header}>
+          <Text style={styles.title}>Playing</Text>
+          <TouchableOpacity>
+            <Text style={{ fontSize: 15, color: "#FECA32" }}>See All </Text>
+          </TouchableOpacity>
+        </View>
+        <MovieCard title="Tenet" />
+      </View>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  bgImage: {
+    width: width,
+    height: height / 1.5,
+    resizeMode: "cover",
+    position: "absolute",
+    top: 0,
+    right: 0,
+  },
+  header: {
+    width: width,
+    marginTop: 100,
+    paddingHorizontal: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 30,
+  },
+  title: {
+    fontSize: 25,
+    color: "#1A2E42",
+  },
+});
 
 export default HomeScreen;
